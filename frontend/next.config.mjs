da20218236f5a10bf6a6d41c76e2a9b8d506f509
Prateek Kumar Srivastav <pankaj111sri@gmail.com>
2026-01-09 01:12:19 +0530

    Remove API proxy rewrites from next.config.mjs
    Removed API proxy rewrites from Next.js configuration.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,6 @@
 
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/proxy/:path*',
-        destination: 'https://jobify-wss2.onrender.com/api/:path*',
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;
