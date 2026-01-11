a1c98294bc3d96f1736fdf366b115018df05d68f
Prateek Kumar Srivastav <pankaj111sri@gmail.com>
2026-01-12 03:13:08 +0530

    Update profile.routes.js

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -5,7 +5,7 @@ import auth from '../middleware/auth.middleware.js';
 const router = express.Router();
 
 
-router.get('/', auth, getProfile);
-router.put('/', auth, updateProfile);
+router.get('/', getProfile);
+router.put('/', updateProfile);
 
 export default router;
